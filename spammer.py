fd8946057e26cd731082a8472c1e7286a929d8d7
contoh028 <70860228+contoh028@users.noreply.github.com>
2020-09-07 03:07:30 +0700

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -2,11 +2,11 @@
 
 # - Spammer v2
 # | Description: spams a phone number by sending it a lot of sms by using Grab API
-# | Author: P4kL0nc4t
+# | Author: ContohQ1
 # | Date: 5/12/2017
 # | Disclaimer: Editing author will not make you the real coder
 
 import spammer_class
 spammer = spammer_class.Spammer()
-spammer.author = "P4kL0nc4t"
+spammer.author = "ContohQ1"
 spammer.main()
